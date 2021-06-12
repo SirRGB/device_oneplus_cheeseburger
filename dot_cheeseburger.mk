6b404ced505bf58190e1c0fb60c09b8a27114e45
karthik990 <knag88@gmail.com>
2021-06-12 16:25:46 +0000

    cheeseburger: Add support for faceunlock

--- dot_cheeseburger.mk
+++ dot_cheeseburger.mk
@@ -21,11 +21,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from cheeseburger device
 $(call inherit-product, device/oneplus/cheeseburger/device.mk)
 
-# Inherit some common DotOS stuff.
-$(call inherit-product, vendor/dot/config/common.mk)
-
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1080
+
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Inherit some common DotOS stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
 
 PRODUCT_NAME := dot_cheeseburger
 PRODUCT_DEVICE := cheeseburger
